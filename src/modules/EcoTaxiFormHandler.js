423635f906ed9d31bed119d4cab1e4d8e8d9ec21
Nikita Suchkov <suchkov8520@gmail.com>
2024-12-10 15:17:41 +0200

    We divided the module's responsibility into FormManager and LocalStorageManager, and also reworked EcoTaxiFormHandler

--- src/modules/EcoTaxiFormHandler.js
+++ src/modules/EcoTaxiFormHandler.js
@@ -1,74 +1,22 @@
 import { EcoTaxi } from "./EcoTaxi";
 
-export class EcoTaxiFormHandler {
-   constructor(baseUrl, board, key, adminkey, encryptionInstance) {
-      this.baseUrl = baseUrl;
-      this.board = board;
-      this.key = key;
-      this.adminkey = adminkey;
-      this.userKeipair = null;
-      this.encryptionManager = encryptionInstance;
-      this.ecoTaxi = new EcoTaxi(baseUrl);
-      this.init();
-   }
-
-   init() {
-      const form = document.getElementById("form");
-      if (form) form.addEventListener("submit", this.handleEvent.bind(this));
-      window.onSubmit = this.handleEvent.bind(this);
-   }
-
-   handleEvent(event) {
-      event.preventDefault();
-
-      const form = event.target;
-      const formData = new FormData(form);
-      const dates = this.collectCheckedDates(formData);
-      const data = this.extractFormData(formData);
-
-      const checks = ["check-1", "check-2", "check-3", "check-4"];
-      const allEmpty = checks.every((check) => !data[check]?.trim());
-
-      if (allEmpty) {
-         this.showErrorMessage("Please provide at least one date in the check fields.");
-         return;
-      }
-
-      if (data.read !== "on") {
-         this.showErrorMessage("Please accept the sorting guidelines.");
-         return;
-      }
-
-      if (!this.validateRecaptcha()) {
-         this.showErrorMessage("Please complete the recaptcha.");
-         return;
-      }
-
-      this.sendData(data, dates);
-   }
-
-   showErrorMessage(message) {
-      const errorElement = document.getElementById("error");
-      errorElement.innerText = message;
-      errorElement?.classList.remove("hidden");
-
-      setTimeout(() => {
-         errorElement?.classList.add("hidden");
-      }, 2000);
-   }
-
-   validateRecaptcha() {
+class FormManager {
+   static validateRecaptcha() {
       const recaptchaResponse = grecaptcha.getResponse();
       return recaptchaResponse.length ? true : false;
    }
 
-   collectCheckedDates(formData) {
+   static validateCheckedDates(formData) {
       const dateFields = ["check-1", "check-2", "check-3", "check-4"];
-      const values = dateFields.filter((field) => formData.get(field)).map((field) => formData.get(field));
-      return values;
+      return dateFields.some((check) => formData[check]?.trim());
    }
 
-   extractFormData(formData) {
+   static collectCheckedDates(formData) {
+      const dateFields = ["check-1", "check-2", "check-3", "check-4"];
+      return dateFields.filter((field) => formData.get(field)).map((field) => formData.get(field));
+   }
+
+   static extractFormData(formData) {
       const data = {};
       formData.forEach((value, key) => {
          if (key === "language") {
@@ -82,12 +30,187 @@ export class EcoTaxiFormHandler {
       });
       return data;
    }
+}
 
-   openModal() {
-      const modal = document.getElementById("modal");
-      const body = document.querySelector("body");
-      modal?.classList.remove("hidden");
-      body?.classList.add("overflow-hidden");
+class LocalStorageManager {
+   static saveDate(key, value) {
+      const jsonData = JSON.stringify(value);
+      localStorage.setItem(key, jsonData);
+   }
+
+   static loadDate(key) {
+      const jsonData = localStorage.getItem(key);
+      return jsonData ? JSON.parse(jsonData) : null;
+   }
+}
+
+export class EcoTaxiFormHandler {
+   constructor(baseUrl, board, key, adminkey, encryptionInstance) {
+      this.ecoTaxi = new EcoTaxi(baseUrl);
+      this.board = board;
+      this.key = key;
+      this.adminkey = adminkey;
+      this.encryptionManager = encryptionInstance;
+      this.#userKeipair = null;
+      this.#localStorageKey = "ecoTaxiFormData";
+
+      this.init();
+   }
+
+   #userKeipair;
+   #localStorageKey;
+
+   init() {
+      const form = document.getElementById("form");
+      if (form) form.addEventListener("submit", this);
+      window.onSubmit = this.handleEvent.bind(this);
+   }
+
+   handleEvent(event) {
+      event.preventDefault();
+
+      const formData = new FormData(event.target);
+      const data = FormManager.extractFormData(formData);
+      const dates = FormManager.collectCheckedDates(formData);
+      const isDateValid = FormManager.validateCheckedDates(data);
+      const isCaptchaValid = FormManager.validateRecaptcha();
+
+      if (!isDateValid) {
+         this.showErrorMessage("Please provide at least one date in the check fields.");
+         return;
+      }
+
+      if (data.read !== "on") {
+         this.showErrorMessage("Please accept the sorting guidelines.");
+         return;
+      }
+
+      if (!isCaptchaValid) {
+         this.showErrorMessage("Please complete the recaptcha.");
+         return;
+      }
+
+      this.sendFormData(data, dates);
+   }
+
+   async sendFormData(data, dates) {
+      const btn = document.getElementById("sendBtn");
+      try {
+         btn.disabled = true;
+         const hasVault = await this.encryptionManager.hasVault();
+
+         if (!hasVault) {
+            this.#userKeipair = this.ecoTaxi.generateUserKeypair();
+            await this.ecoTaxi.registerUser(data.name, this.#userKeipair);
+            data.chat = this.ecoTaxi.buildUserLink(this.#userKeipair);
+            await this.ecoTaxi.sendMessage(this.#userKeipair, this.adminkey, this.generateText(data, dates));
+
+            LocalStorageManager.saveDate(this.#localStorageKey, {
+               userName: data.name,
+               userKeipair: { privateKey: this.#userKeipair.privateKey, publicKey: this.#userKeipair.publicKey },
+               userData: data,
+            });
+         }
+
+         if (hasVault && !this.encryptionManager.isAuth) {
+            this.#userKeipair = this.ecoTaxi.generateUserKeypair();
+            await this.ecoTaxi.registerUser(data.name, this.#userKeipair);
+            await this.ecoTaxi.sendMessage(this.#userKeipair, this.adminkey, this.generateText(data, dates));
+
+            LocalStorageManager.saveDate(this.#localStorageKey, {
+               userName: data.name,
+               userKeipair: { privateKey: this.#userKeipair.privateKey, publicKey: this.#userKeipair.publicKey },
+               userData: data,
+            });
+         }
+
+         if (hasVault && this.encryptionManager.isAuth) {
+            const encryptedData = await this.encryptionManager.getData();
+            const parsedData = JSON.parse(encryptedData);
+
+            if (parsedData?.userData) {
+               parsedData.userData = { ...parsedData.userData, ...data };
+               this.#userKeipair = parsedData.userKeipair;
+               await this.ecoTaxi.sendMessage(this.#userKeipair, this.adminkey, this.generateText(data, dates));
+               const updatedData = JSON.stringify(parsedData);
+               await this.encryptionManager.setData(updatedData);
+            } else {
+               const parsedData = JSON.parse(encryptedData);
+               this.#userKeipair = parsedData.userKeipair;
+               await this.ecoTaxi.sendMessage(this.#userKeipair, this.adminkey, this.generateText(data, dates));
+               const updatedData = JSON.stringify({ userData: data, userKeipair: this.#userKeipair, userName: data.name });
+               await this.encryptionManager.setData(updatedData);
+            }
+         }
+
+         const values = this.prepareRequestPayload(data, dates);
+
+         // let result;
+         // const response = await this.sendMonday(values);
+         // result = await response.json();
+
+         // if (data && data.file && data.file.size > 0) {
+         //    try {
+         //       await this.uploadFileMonday(result.data.create_item.id, data.file);
+         //    } catch (error) {
+         //       console.error("Ошибка при загрузке файла:", error);
+         //       throw new Error("Не удалось загрузить файл.");
+         //    }
+         // }
+
+         this.openModal();
+      } catch (error) {
+         console.error("Ошибка при обработке формы:", error);
+      } finally {
+         btn.disabled = false;
+         grecaptcha.reset();
+      }
+   }
+
+   async sendMonday(values) {
+      return await fetch("https://api.monday.com/v2", {
+         method: "post",
+         headers: {
+            "Content-Type": "application/json",
+            "Authorization": this.key,
+         },
+         body: JSON.stringify({
+            query: `
+                       mutation ($myItemName: String!, $columnVals: JSON!) {
+                           create_item (board_id: ${this.board}, item_name: $myItemName, column_values: $columnVals) {
+                               id
+                           }
+                       }
+                   `,
+            variables: values,
+         }),
+      });
+   }
+
+   async uploadFileMonday(itemId, file) {
+      const formData = new FormData();
+      formData.append(
+         "query",
+         `
+            mutation ($file: File!) {
+                add_file_to_column (item_id: ${itemId}, column_id: "files", file: $file) {
+                    id
+                }
+            }
+        `,
+      );
+      formData.append("variables[file]", file);
+
+      const fileResponse = await fetch("https://api.monday.com/v2/", {
+         method: "POST",
+         headers: {
+            Authorization: this.key,
+         },
+         body: formData,
+      });
+
+      const fileResult = await fileResponse.json();
+      console.log("Ответ сервера при загрузке файла:", fileResult);
    }
 
    generateText(data, dates) {
@@ -133,175 +256,20 @@ export class EcoTaxiFormHandler {
       };
    }
 
-   saveToLocalStorage(key, value) {
-      try {
-         const jsonData = JSON.stringify(value);
-         localStorage.setItem(key, jsonData);
-         console.log(`Данные успешно сохранены в localStorage под ключом "${key}".`);
-      } catch (error) {
-         console.error("Ошибка при сохранении данных в localStorage:", error);
-      }
+   showErrorMessage(message) {
+      const errorElement = document.getElementById("error");
+      errorElement.innerText = message;
+      errorElement?.classList.remove("hidden");
+
+      setTimeout(() => {
+         errorElement?.classList.add("hidden");
+      }, 2500);
    }
 
-   loadFromLocalStorage(key) {
-      try {
-         const jsonData = localStorage.getItem(key);
-         if (!jsonData) {
-            console.warn(`Данные с ключом "${key}" отсутствуют в localStorage.`);
-            return null;
-         }
-         return JSON.parse(jsonData);
-      } catch (error) {
-         console.error("Ошибка при загрузке данных из localStorage:", error);
-         return null;
-      }
-   }
-
-   async sendData(data, dates) {
-      const btn = document.getElementById("sendBtn");
-      try {
-         btn.disabled = true;
-         const localStorageKey = "ecoTaxiFormData";
-         const text = this.generateText(data, dates);
-
-         const hasVault = await this.encryptionManager.hasVault();
-
-         if (hasVault && !this.encryptionManager.isAuth) {
-            this.userKeipair = this.ecoTaxi.generateUserKeypair();
-
-            // Регистрируем пользователя
-            await this.ecoTaxi.registerUser(data.name, this.userKeipair);
-
-            // Отправляем сообщение
-            await this.ecoTaxi.sendMessage(this.userKeipair, this.adminkey, text);
-
-            // Сохраняем данные в локальное хранилище
-            this.saveToLocalStorage(localStorageKey, { data, ...this.userKeipair });
-         }
-
-         if (!hasVault) {
-            // Сохраняем данные только в незашифрованное хранилище
-            this.userKeipair = this.ecoTaxi.generateUserKeypair();
-
-            // Регистрируем пользователя
-            await this.ecoTaxi.registerUser(data.name, this.userKeipair);
-
-            // Генерируем ссылку и добавляем в данные
-            const link = this.ecoTaxi.buildUserLink(this.userKeipair);
-            data.chat = link;
-
-            // Отправляем сообщение
-            await this.ecoTaxi.sendMessage(this.userKeipair, this.adminkey, text);
-
-            // Сохраняем данные в локальное хранилище
-            this.saveToLocalStorage(localStorageKey, { data, ...this.userKeipair });
-         }
-
-         if (hasVault && this.encryptionManager.isAuth) {
-            // Работаем с зашифрованным хранилищем
-            const encryptedData = await this.encryptionManager.getData();
-
-            if (encryptedData) {
-               const parsedData = JSON.parse(encryptedData);
-
-               if (parsedData.userData) {
-                  // Обновляем данные пользователя
-                  parsedData.userData = { ...parsedData.userData, ...data };
-
-                  // Отправляем сообщение
-                  this.userKeipair = parsedData.userKeipair;
-
-                  await this.ecoTaxi.sendMessage(this.userKeipair, this.adminkey, text);
-                  // Сохраняем обновленные данные в зашифрованное хранилище
-                  const updatedData = JSON.stringify(parsedData);
-                  await this.encryptionManager.setData(updatedData);
-               } else {
-                  const parsedData = JSON.parse(encryptedData);
-
-                  this.userKeipair = JSON.parse(parsedData.userKeipair);
-
-                  await this.ecoTaxi.sendMessage(this.userKeipair, this.adminkey, text);
-
-                  const updatedData = JSON.stringify({ userData: data, userKeipair: this.userKeipair });
-                  await this.encryptionManager.setData(updatedData);
-               }
-
-               console.log("Данные обновлены в зашифрованном хранилище.");
-            } else {
-               console.warn("Не удалось найти данные в зашифрованном хранилище.");
-            }
-         }
-
-         const values = this.prepareRequestPayload(data, dates);
-
-         // 5. Отправка данных в Monday.com API
-         let result;
-         const response = await this.sendMonday(values);
-         result = await response.json();
-
-         // 7. Загрузка файла, если он указан
-         if (data && data.file && data.file.size > 0) {
-            try {
-               await this.uploadFileMonday(result.data.create_item.id, data.file);
-            } catch (error) {
-               console.error("Ошибка при загрузке файла:", error);
-               throw new Error("Не удалось загрузить файл.");
-            }
-         }
-
-         // 8. Открытие модального окна
-         this.openModal();
-      } catch (error) {
-         console.error("Ошибка при обработке формы:", error);
-      } finally {
-         btn.disabled = false;
-         grecaptcha.reset();
-      }
-   }
-
-   async sendMonday(values) {
-      return await fetch("https://api.monday.com/v2", {
-         method: "post",
-         headers: {
-            "Content-Type": "application/json",
-            "Authorization": this.key,
-         },
-         body: JSON.stringify({
-            query: `
-                    mutation ($myItemName: String!, $columnVals: JSON!) {
-                        create_item (board_id: ${this.board}, item_name: $myItemName, column_values: $columnVals) {
-                            id
-                        }
-                    }
-                `,
-            variables: values,
-         }),
-      });
-   }
-
-   async uploadFileMonday(itemId, file) {
-      const formData = new FormData();
-      formData.append(
-         "query",
-         `
-            mutation ($file: File!) {
-                add_file_to_column (item_id: ${itemId}, column_id: "files", file: $file) {
-                    id
-                }
-            }
-        `,
-      );
-      formData.append("variables[file]", file);
-
-      const fileResponse = await fetch("https://api.monday.com/v2/", {
-         method: "POST",
-         headers: {
-            Authorization: this.key,
-         },
-         body: formData,
-      });
-
-      const fileResult = await fileResponse.json();
-      console.log("Ответ сервера при загрузке файла:", fileResult);
+   openModal() {
+      const modal = document.getElementById("modal");
+      const body = document.querySelector("body");
+      modal?.classList.remove("hidden");
+      body?.classList.add("overflow-hidden");
    }
 }
